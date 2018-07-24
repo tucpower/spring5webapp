bd025a63b8b1fe1b8e91e40f4f31e4e9c3339549
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-07-24 16:34:41 +0300

    Completed Section 20.

--- src/main/java/com/jrt/spring5webapp/model/Book.java
+++ src/main/java/com/jrt/spring5webapp/model/Book.java
@@ -12,7 +12,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -22,17 +24,10 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
-    }
-
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
-        this.title = title;
-        this.isbn = isbn;
-        this.publisher = publisher;
-        this.authors = authors;
     }
 
     public Long getId() {
@@ -59,11 +54,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
